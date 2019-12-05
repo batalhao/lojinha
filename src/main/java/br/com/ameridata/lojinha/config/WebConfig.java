3db011e59e5b4b7b3746c5ad1dae587de3e4d296
Paulo R. Batalhão <paulo@javahub.dev>
2019-12-05 08:09:30 -0300

    Inicio do cadastro de produtos (Lojinha Web)
    - Adicionada configurações adicionais no pom.xml
    - Atualizado AppInitializer e WebConfig
    - Classe do Contoller de Produtos atualizada.

--- src/main/java/br/com/ameridata/lojinha/config/WebConfig.java
+++ src/main/java/br/com/ameridata/lojinha/config/WebConfig.java
@@ -1,15 +1,64 @@
 package br.com.ameridata.lojinha.config;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.ISpringTemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import br.com.ameridata.lojinha.controller.ProdutosController;
 
 @Configuration
 @ComponentScan(basePackageClasses = { ProdutosController.class })
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer /* extends WebMvcConfigurerAdapter */ {
+public class WebConfig implements ApplicationContextAware, WebMvcConfigurer /* extends WebMvcConfigurerAdapter */ {
+
+	private ApplicationContext applicationContext;
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+	}
+
+	@Bean
+	public ViewResolver viewResolver() {
+		ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+
+		resolver.setTemplateEngine(templateEngine());
+		resolver.setCharacterEncoding("UTF-8");
+
+		return resolver;
+	}
+
+	@Bean
+	public ISpringTemplateEngine /* TemplateEngine */ templateEngine() {
+		SpringTemplateEngine engine = new SpringTemplateEngine();
+
+		engine.setEnableSpringELCompiler(true);
+		engine.setTemplateResolver(templateResolver());
+
+		return engine;
+	}
+
+	private ITemplateResolver templateResolver() {
+		SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+
+		resolver.setApplicationContext(this.applicationContext);
+		resolver.setPrefix("classpath:/templates/");
+		resolver.setSuffix(".html");
+		resolver.setTemplateMode(TemplateMode.HTML);
+
+		return resolver;
+	}
 
 }
--- src/main/java/br/com/ameridata/lojinha/controller/ProdutosController.java
+++ src/main/java/br/com/ameridata/lojinha/controller/ProdutosController.java
@@ -1,8 +1,14 @@
 package br.com.ameridata.lojinha.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ProdutosController {
+
+	@RequestMapping("/produtos/novo")
+	public String novo() {
+		return "produto/CadastroProduto";
+	}
 
 }
