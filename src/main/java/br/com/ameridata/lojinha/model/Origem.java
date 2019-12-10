a2cba058b72b57aa31e2ac71f8405c4f388f953a
Paulo R. Batalhão <paulo@javahub.dev>
2019-12-10 13:39:02 -0300

    Criação do enum de Origem e Unidade (Lojinha Web)

---
+++ src/main/java/br/com/ameridata/lojinha/model/Origem.java
@@ -0,0 +1,17 @@
+package br.com.ameridata.lojinha.model;
+
+public enum Origem {
+
+	NACIONAL("Nacional"), INTERNACIONAL("Internacional");
+
+	private String descricao;
+
+	Origem(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return this.descricao;
+	}
+
+}
