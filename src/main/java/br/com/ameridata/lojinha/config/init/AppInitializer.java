388b61659af7fee2ea43e789e588a973089cfc84
Paulo R. Batalhão <paulo@javahub.dev>
2019-12-13 19:09:25 -0300

    Adicionado o JPAConfig no Initializer (Lojinha Web)

--- src/main/java/br/com/ameridata/lojinha/config/init/AppInitializer.java
+++ src/main/java/br/com/ameridata/lojinha/config/init/AppInitializer.java
@@ -5,13 +5,14 @@ import javax.servlet.Filter;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import br.com.ameridata.lojinha.config.JPAConfig;
 import br.com.ameridata.lojinha.config.WebConfig;
 
 public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		return null;
+		return new Class<?>[] { JPAConfig.class };
 	}
 
 	@Override
