294ee238e26e776bceb4b01a986052da54d46206
Paulo R. Batalhão <paulo@javahub.dev>
2019-12-15 08:35:37 -0300

    Adicionado inserções com FlyWay em Fabricantes e Fornecedores (Lojinha Web)

---
+++ src/main/resources/db/migration/V002__inserir_valores_tabelas_fornecedores_e_fabricantes.sql
@@ -0,0 +1,14 @@
+insert into fabricantes(nome, ativo) values('Royal Canin', b'1');
+insert into fabricantes(nome, ativo) values('Pedigree', b'1');
+insert into fabricantes(nome, ativo) values('Golden', b'1');
+insert into fabricantes(nome, ativo) values('Gran Plus', b'0');
+insert into fabricantes(nome, ativo) values('ND', b'0');
+insert into fabricantes(nome, ativo) values('PremieR', b'1');
+insert into fabricantes(nome, ativo) values('Faro', b'1');
+
+insert into fornecedores(nome, ativo) values('Super Pet Americana', b'1');
+insert into fornecedores(nome, ativo) values('Petland', b'0');
+insert into fornecedores(nome, ativo) values('Petz Piracicaba', b'1');
+insert into fornecedores(nome, ativo) values('Cobasi', b'1');
+insert into fornecedores(nome, ativo) values('Pet Center', b'0');
+insert into fornecedores(nome, ativo) values('Amicão', b'1');
