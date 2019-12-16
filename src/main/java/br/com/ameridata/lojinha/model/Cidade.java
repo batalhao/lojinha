38c5db534c5cdbb5a7a6d733bef40d38696cc53b
Paulo R. Batalhão <paulo@javahub.dev>
2019-12-16 20:42:04 -0300

    Implementação do cadastro de cidades (Lojinha Web)

--- src/main/java/br/com/ameridata/lojinha/model/Cidade.java
+++ src/main/java/br/com/ameridata/lojinha/model/Cidade.java
@@ -1,6 +1,7 @@
 package br.com.ameridata.lojinha.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,6 +11,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "cidades")
@@ -21,10 +23,12 @@ public class Cidade implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
-	@NotBlank(message = "Nome é obrigatório")
+	@NotBlank(message = "Nome: Campo obrigatório.")
+	@Size(max = 50, message = "Nome: Tamanho máximo de 50 caracteres.")
 	private String nome;
 
 	@ManyToOne()
+	@NotBlank(message = "Estado: Campo obrigatório.")
 	@JoinColumn(name = "estado_id")
 	private Estado estado;
 
@@ -48,12 +52,13 @@ public class Cidade implements Serializable {
 		return id;
 	}
 
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
+		return Objects.hash(id);
 	}
 
 	@Override
@@ -65,12 +70,7 @@ public class Cidade implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Cidade other = (Cidade) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
+		return Objects.equals(id, other.id);
 	}
 
 }
