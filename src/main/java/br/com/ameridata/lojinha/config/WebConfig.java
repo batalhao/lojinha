c658249f4e975aa98d63b5fb718c31b23de1d390
Paulo R. Batalhão <paulo@javahub.dev>
2019-12-16 14:55:58 -0300

    Implementado o NumberStyleFormatter (Lojinha Web)

--- src/main/java/br/com/ameridata/lojinha/config/WebConfig.java
+++ src/main/java/br/com/ameridata/lojinha/config/WebConfig.java
@@ -1,17 +1,23 @@
 package br.com.ameridata.lojinha.config;
 
+import java.math.BigDecimal;
+import java.util.Locale;
+
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.number.NumberStyleFormatter;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 import org.thymeleaf.spring5.ISpringTemplateEngine;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
@@ -20,6 +26,7 @@ import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import br.com.ameridata.lojinha.controller.ProdutosController;
+import br.com.ameridata.lojinha.controller.converter.EmpresaConverter;
 import br.com.ameridata.lojinha.controller.converter.FabricanteConverter;
 import br.com.ameridata.lojinha.controller.converter.FornecedorConverter;
 import nz.net.ultraq.thymeleaf.LayoutDialect;
@@ -79,7 +86,20 @@ public class WebConfig implements ApplicationContextAware, WebMvcConfigurer /* e
 		DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
 		conversionService.addConverter(new FornecedorConverter());
 		conversionService.addConverter(new FabricanteConverter());
+		conversionService.addConverter(new EmpresaConverter());
+
+		NumberStyleFormatter bigDecimalFormatter = new NumberStyleFormatter("#,##0.00");
+		conversionService.addFormatterForFieldType(BigDecimal.class, bigDecimalFormatter);
+
+		NumberStyleFormatter integerFormatter = new NumberStyleFormatter("#,##0");
+		conversionService.addFormatterForFieldType(Integer.class, integerFormatter);
 
 		return conversionService;
 	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		return new FixedLocaleResolver(new Locale("pt", "BR"));
+	}
+
 }
