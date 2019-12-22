e10364ff7fd470983bb604d4922cf911370c25d5
Paulo R. Batalhão <paulo@javahub.dev>
2019-12-22 12:42:47 -0300

    Modularização do Lojinha.js

--- src/main/resources/static/javascripts/lojinha.js
+++ src/main/resources/static/javascripts/lojinha.js
@@ -1,7 +1,22 @@
-$(function () {
-	var decimal = $('.js-decimal');
-	decimal.maskMoney({ precision: 2, thousands: ".", decimal: "," });
+var Lojinha = Lojinha || {};
 
-	var plain = $('.js-plain');
-	plain.maskMoney({ precision: 0, thousands: "." });
-});
+Lojinha.MaskMoney = (function () {
+
+	function MaskMoney() {
+		this.decimal = $('.js-decimal');
+		this.plain = $('.js-plain');
+	}
+
+	MaskMoney.prototype.enable = function () {
+		this.decimal.maskMoney({ precision: 2, thousands: ".", decimal: ",", allowZero: true });
+		this.plain.maskMoney({ precision: 0, thousands: "", allowZero: true });
+	}
+
+	return MaskMoney;
+
+}());
+
+$(function () {
+	var maskMoney = new Lojinha.MaskMoney();
+	maskMoney.enable();
+}); 
