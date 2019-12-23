6fd202dd5a3c24838daba44d83bccf69dfd51385
Paulo R. Batalhão <paulo@javahub.dev>
2019-12-23 15:46:38 -0300

    Adicionado Bean para FotoStorage, chamando FotoStorageLocal

--- src/main/java/br/com/ameridata/lojinha/config/ServiceConfig.java
+++ src/main/java/br/com/ameridata/lojinha/config/ServiceConfig.java
@@ -1,12 +1,20 @@
 package br.com.ameridata.lojinha.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import br.com.ameridata.lojinha.service.CadastroProdutoService;
+import br.com.ameridata.lojinha.storage.FotoStorage;
+import br.com.ameridata.lojinha.storage.local.FotoStorageLocal;
 
 @Configuration
 @ComponentScan(basePackageClasses = CadastroProdutoService.class)
 public class ServiceConfig {
+
+	@Bean
+	public FotoStorage fotoStorage() {
+		return new FotoStorageLocal();
+	}
 
 }
