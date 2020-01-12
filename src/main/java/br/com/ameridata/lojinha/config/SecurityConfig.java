52d5f22bd17c6f70117c99a7dc53815ca31f6c6d
Paulo R. Batalhão <paulo@javahub.dev>
2020-01-12 12:43:01 -0300

    Continuando com a implementação do Spring Security e tela de Login

--- src/main/java/br/com/ameridata/lojinha/config/SecurityConfig.java
+++ src/main/java/br/com/ameridata/lojinha/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package br.com.ameridata.lojinha.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,6 +12,22 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 //@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("admin").password("admin").roles("CADASTRO_CLIENTE");
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/layout/**").antMatchers("/images/**");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and()
+				.csrf().disable();
+	}
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
