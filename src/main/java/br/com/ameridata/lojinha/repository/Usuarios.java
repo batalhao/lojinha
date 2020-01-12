384dd63f8346e84bbd05f7872915568d3a56ef82
Paulo R. Batalhão <paulo@javahub.dev>
2020-01-12 15:05:21 -0300

    Implementação de busca no banco por email do usuário.

--- src/main/java/br/com/ameridata/lojinha/repository/Usuarios.java
+++ src/main/java/br/com/ameridata/lojinha/repository/Usuarios.java
@@ -8,8 +8,10 @@ import org.springframework.stereotype.Repository;
 import br.com.ameridata.lojinha.model.Usuario;
 
 @Repository
-public interface Usuarios extends JpaRepository<Usuario, Long> {
+public interface Usuarios extends JpaRepository<Usuario, Long> /* , UsuariosQueries */ {
 
 	public Optional<Usuario> findByEmail(String email);
+
+	public Optional<Usuario> findByEmailIgnoreCaseAndAtivoTrue(String email);
 
 }
