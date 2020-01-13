1f7c23b30e60ef79938b58f3e324592206366b0c
Paulo R. Batalhão <paulo@javahub.dev>
2020-01-13 13:33:37 -0300

    Adicionada saudação no cabeçalho das páginas

--- src/main/java/br/com/ameridata/lojinha/security/UsuarioSistema.java
+++ src/main/java/br/com/ameridata/lojinha/security/UsuarioSistema.java
@@ -1,5 +1,6 @@
 package br.com.ameridata.lojinha.security;
 
+import java.util.Calendar;
 import java.util.Collection;
 
 import org.springframework.security.core.GrantedAuthority;
@@ -21,6 +22,39 @@ public class UsuarioSistema extends User {
 
 	public Usuario getUsuario() {
 		return usuario;
+	}
+
+	public String getSaudacao() {
+		Calendar data = Calendar.getInstance();
+		int hora = data.get(Calendar.HOUR_OF_DAY);
+
+		String saudacao = "Olá " + this.usuario.getNome();
+		boolean aniversario = verificaAniversario(data);
+
+		if (aniversario) {
+			return saudacao + ", feliz aniversário!";
+		}
+
+		if (hora >= 18 && hora < 24) {
+			return saudacao + ", boa noite!";
+		} else if (hora >= 12 && hora < 18) {
+			return saudacao + ", boa tarde!";
+		} else if (hora >= 0 && hora < 12) {
+			return saudacao + ", bom dia!";
+		} else {
+			return saudacao;
+		}
+	}
+
+	private boolean verificaAniversario(Calendar data) {
+		if (this.usuario.getDataNascimento() != null) {
+			boolean mesmoMes = this.usuario.getDataNascimento().getDayOfMonth() == data.get(Calendar.DAY_OF_MONTH);
+			boolean mesmoDia = this.usuario.getDataNascimento().getMonthValue() == data.get(Calendar.MONTH) + 1;
+
+			return mesmoMes && mesmoDia;
+		} else {
+			return false;
+		}
 	}
 
 }
