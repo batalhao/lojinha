9de7b27f33df20e42b9d6410ac56905a8d19ee4c
Paulo R. Batalhão <paulo@javahub.dev>
2020-01-20 15:05:53 -0300

    Identação  IDEA

--- src/main/java/br/com/ameridata/lojinha/model/Cidade.java
+++ src/main/java/br/com/ameridata/lojinha/model/Cidade.java
@@ -1,24 +1,20 @@
 package br.com.ameridata.lojinha.model;
 
-import java.io.Serializable;
-import java.util.Objects;
+import com.google.gson.annotations.Expose;
+import lombok.Getter;
+import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
-import com.google.gson.annotations.Expose;
+import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
 @Table(name = "cidades")
+@Getter
+@Setter
 public class Cidade implements Serializable {
 
     private static final long serialVersionUID = -6898722947358560393L;
@@ -38,30 +34,6 @@ public class Cidade implements Serializable {
     @JoinColumn(name = "estado_id")
 //	@JsonIgnore
     private Estado estado;
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public Estado getEstado() {
-        return estado;
-    }
-
-    public void setEstado(Estado estado) {
-        this.estado = estado;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
 
     public boolean hasEstado() {
         return this.estado != null;
