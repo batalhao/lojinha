74af2ea696ef078e54a735819697a73c0c1687ce
Paulo R. Batalhão <paulo@javahub.dev>
2020-01-27 17:12:09 -0300

    Finalizando  controle de checkbox  para tela de pesquisa de Usuários

--- src/main/resources/static/javascripts/multiselecao.js
+++ src/main/resources/static/javascripts/multiselecao.js
@@ -5,15 +5,19 @@ Lojinha.MultiSelecao = (function() {
     function MultiSelecao() {
         this.statusBtn = $('.js-status-btn');
         this.selecaoCheckbox = $('.js-selecao');
+        this.selecaoTodosCheckbox = $('.js-selecao-todos');
     }
 
     MultiSelecao.prototype.iniciar = function() {
         this.statusBtn.on('click', onStatusBtnClicado.bind(this));
+        this.selecaoTodosCheckbox.on('click', onSelecaoTodosClicado.bind(this));
+        this.selecaoCheckbox.on('click', onSelecaoClicado.bind(this));
     }
 
     function onStatusBtnClicado(event) {
         var botaoClicado = $(event.currentTarget);
         var status = botaoClicado.data('status');
+        var uri = botaoClicado.data('url')
 
         var checkBoxSelecionados = this.selecaoCheckbox.filter(':checked');
         var ids = $.map(checkBoxSelecionados, function(c) {
@@ -22,7 +26,7 @@ Lojinha.MultiSelecao = (function() {
 
         if (ids.length > 0) {
             $.ajax({
-                url: '/lojinha/usuarios/status',
+                url: uri,
                 method: 'PUT',
                 data: {
                     ids: ids,
@@ -34,6 +38,22 @@ Lojinha.MultiSelecao = (function() {
             });
 
         }
+    }
+
+    function onSelecaoTodosClicado () {
+        var status = this.selecaoTodosCheckbox.prop('checked');
+        this.selecaoCheckbox.prop('checked', status);
+        statusBotaoAcao.call(this, status);
+    }
+
+    function onSelecaoClicado () {
+        var selecaoCheckboxChecados = this.selecaoCheckbox.filter(':checked');
+        this.selecaoTodosCheckbox.prop('checked', selecaoCheckboxChecados.length >= this.selecaoCheckbox.length)
+        statusBotaoAcao.call(this, selecaoCheckboxChecados.length);
+    }
+
+    function statusBotaoAcao (ativar) {
+        ativar ? this.statusBtn.removeClass('disabled') : this.statusBtn.addClass('disabled');
     }
 
     return MultiSelecao;
