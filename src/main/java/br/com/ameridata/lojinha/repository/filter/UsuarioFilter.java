bef28dd6c3f51fc59412c4e0645f6b3c8b6414f3
Paulo R. Batalhão <paulo@javahub.dev>
2020-01-27 09:38:38 -0300

    Novas unidades adicionadas para a pesquisa de usuários

---
+++ src/main/java/br/com/ameridata/lojinha/repository/filter/UsuarioFilter.java
@@ -0,0 +1,13 @@
+package br.com.ameridata.lojinha.repository.filter;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UsuarioFilter {
+
+    private String nome;
+    private String email;
+
+}
